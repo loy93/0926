c3d6abfde99c233e602c26bcd852df910ffdd96d
loy93 <loy93@users.noreply.github.com>
2018-09-26 16:11:37 +0800

    注释

---
+++ src/BankATM.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BankATM {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("欢迎使用中国人民很行ATM系统");
+        System.out.println("1.进入  2.退出");
+        int inPut = scanner.nextInt();
+
+        if (inPut == 1){
+
+        }else {}
+
+
+    }
+}
---
+++ src/user/User.java
@@ -0,0 +1,52 @@
+package user;
+
+public class User {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    public String getCardNo() {
+        return cardNo;
+    }
+
+    public void setCardNo(String cardNo) {
+        this.cardNo = cardNo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String name;
+    private String balance;
+    private String cardNo;
+    private String password;
+
+
+}
